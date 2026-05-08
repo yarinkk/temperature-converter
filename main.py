32ec21e9ae499beddf7070ba9e3a674ff66f6c37
yarinkk <130041657+yarinkk@users.noreply.github.com>
2026-05-08 13:58:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import sentry_sdk
 from app.converter import celsius_to_fahrenheit
+
+sentry_sdk.init(
+    dsn="https://d27258db425273c83ad2628bee2c90cd@o4511296908427264.ingest.de.sentry.io/4511296918257744",
+    traces_sample_rate=1.0,
+)
 
 
 def main():
@@ -11,7 +17,9 @@ def main():
 
         print(f"Температура у Фаренгейтах: {fahrenheit}")
 
-    except ValueError:
+    except ValueError as e:
+        sentry_sdk.capture_exception(e)
+        sentry_sdk.flush()
         print("Помилка: введіть коректне число")
 
 
